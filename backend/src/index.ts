227919aca0b28ba6906bc8ffcbc8dceed4ca3d91
Robert Paleno <43076160+rpaleno@users.noreply.github.com>
2024-09-05 13:03:21 -0700

    added user route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,14 @@ import express, { Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose"
-
+import myUserRoute from "./routes/MyUserRoute";
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to the database"))
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response)=>{
-    res.json({ message: "Hello!" });
-})
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, ()=> {
     console.log("server started on localhost: 7000");
